878c4d7b23db11f70d379b9d605387efedc3c8f1
Gabriele Merra <gabriele.merra@alkemy.com>
2019-02-10 11:41:57 +0100

    first

---
+++ src/pages/Demo2.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class Demo2 extends React.Component{
+  render() {
+    return (
+      <div>
+        Ciao Zozzone Numero Due!!!!
+      </div>
+    )
+  }
+}
+
+export default Demo2
